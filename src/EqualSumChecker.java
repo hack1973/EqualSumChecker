4782f1f2ebed485bf0d8aad90c6975fbf440e4a8
Michael Hacker <hack1973@swbell.net>
2021-09-04 14:09:52 -0500

    Initial commit

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum (int firstInt, int secondInt, int thirdInt) {
+
+        if (firstInt + secondInt == thirdInt) {
+            return true;
+        }
+        return false;
+    }
+}
